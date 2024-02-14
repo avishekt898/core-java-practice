f3e3a0386f4445eb826560874a21983c98a2a8be
avishekt898 <avishekt898@gmail.com>
2024-02-13 20:40:37 -0600

    changes in welcome

--- src/Welcome.java
+++ src/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
         System.out.println(greeting);
         for (int i=0;i<greeting.length();i++){
             System.out.print("=");
-   t     }
+        }
         System.out.println();
     }
 }
