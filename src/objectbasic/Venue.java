0c0cfe1842bd085df8a6172d5a047e7752596c24
avishekt898 <avishekt898@gmail.com>
2024-02-15 19:12:38 -0600

    venue added

---
+++ src/objectbasic/Venue.java
@@ -0,0 +1,20 @@
+package objectbasic;
+
+public class Venue
+{/*
+venue name,
+location,
+capacity,
+main hall,
+parking,
+furniture,
+meeting rooms,
+catering facilities,
+Wi-Fi and connectivity,
+security,
+cost,
+decoration,
+secuirty,
+restroom,
+*/
+}
