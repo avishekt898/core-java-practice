ca98a05a0f90357ad2e1cb7835f24d6df960c82d
avishekt898 <avishekt898@gmail.com>
2024-02-19 02:05:43 -0600

    made new object

---
+++ src/objectbasic/himalayantimes/CovidConnect.java
@@ -0,0 +1,16 @@
+package objectbasic.himalayantimes;
+
+public class CovidConnect {
+    boolean isCovid19HelpDesksAvailable;
+    boolean isVaccineAvailableToPersonWIthDisabilities;
+    boolean isThere24hrCovidHotlineService;
+
+    @Override
+    public String toString() {
+        return "CovidConnect{" +
+                "isCovid19HelpDesksAvailable=" + isCovid19HelpDesksAvailable +
+                ", isVaccineAvailableToPersonWIthDisabilities=" + isVaccineAvailableToPersonWIthDisabilities +
+                ", isThere24hrCovidHotlineService=" + isThere24hrCovidHotlineService +
+                '}';
+    }
+}
